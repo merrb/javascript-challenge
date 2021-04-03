2cceb01748cd44edd70ea1b9fe4d3d892a8a200f
billye <billyemerritts@gmail.com>
2021-04-03 15:27:29 -0400

    changes

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -15,4 +15,24 @@ tableData.forEach(UFOdata => {
         cell.text(value);
     });
 }); 
+
+//apply filter
+var button = d3.select("#filter-btn");
+
+//define where the button is located
+button.on("click", function(){
+    d3.event.preventDefault();
+    tbody.selectAll('#').remove();
+    var inputDate = d3.select("#datetime")
+    var inputText = inputDate.property("value")
+    var filteredData = tableData.filter(x => x.datetime === inputText);
+
+    filteredData.forEach(function(UFOdata) {
+        var row = tbody.append("tr")
+        Object.enters(UFOdata).forEach(function([key,value]) {
+            var cell = tbody.append("td");
+            cell.text(value)
+        })
+    })
+})
     
