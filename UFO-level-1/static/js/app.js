c4ba73dc9c51634a857849cbcbf05667fd541490
billye <billyemerritts@gmail.com>
2021-04-03 15:10:02 -0400

    changes

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Level 1: Creating a table and date search
+
+//var tableData = data;
+
+//creating the tbody
+var tbody = d3.select("tbody");
+tableData.forEach(UFOdata => {
+    var row = tbody.append("tr");
+    Object.entries(UFOdata).forEach(function([key,value]){
+        var cell = row.append("td");
+        cell.text(value);
+    });
+}); 
+    
